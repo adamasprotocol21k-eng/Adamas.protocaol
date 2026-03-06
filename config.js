5399d495fd138cc5ca7c44c0dbf2874cae9d481b
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-06 14:31:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 // Adamas Protocol Master Configuration
-const MASTER_CONFIG = {
-    firebase: {
-        apiKey: "YOUR_API_KEY", // Firebase Console se copy karein
-        authDomain: "adamas-protocol.firebaseapp.com",
-        projectId: "adamas-protocol",
-        storageBucket: "adamas-protocol.appspot.com",
-        messagingSenderId: "YOUR_SENDER_ID",
-        appId: "YOUR_APP_ID"
-    },
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ2i6r8F66CxKpnbwMEhPS4pwC36V0Kgg",
+  authDomain: "adamas-protocol.firebaseapp.com",
+  databaseURL: "https://adamas-protocol-default-rtdb.firebaseio.com",
+  projectId: "adamas-protocol",
+  storageBucket: "adamas-protocol.firebasestorage.app",
+  messagingSenderId: "207788425238",
+  appId: "1:207788425238:web:025b8544f085dde60af537",
+  measurementId: "G-NVCWQ1XQZS"
+};
     blockchain: {
         contractAddress: "0x6DbC17D9950e0b3A7627ec6bFc6b210A998da690", // Aapka ABP Smart Contract [cite: 2026-02-26]
         minWithdraw: 5000, // Kam se kam withdrawal limit [cite: 2026-02-24]
