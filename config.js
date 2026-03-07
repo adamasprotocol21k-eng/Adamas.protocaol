4e5427cbc5813e3fb89f3d26d5e7fda882373770
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-07 16:55:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+// Maintenance Mode
+
+const MAINTENANCE_MODE = true;
+// true = site maintenance
+// false = site live
+
 const CONTRACT_ADDRESS="0x6DbC17D9950e0b3A7627ec6bFc6b210A998da690";
 
 const CHAIN_ID=80002;
