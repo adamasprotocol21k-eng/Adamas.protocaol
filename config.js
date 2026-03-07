0debec550dcc9909d3f15bf3625a89caa30c1ee7
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-07 19:02:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,22 @@
-const CONFIG = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-twitter: "YOUR_TWITTER_LINK",
-
-telegram: "YOUR_TELEGRAM_LINK",
-
-contract: "YOUR_CONTRACT_ADDRESS",
-
-dailyMin: 10,
-dailyMax: 1000,
-
-lotteryPrice: 1000,
-
-refPercent: 10
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ2i6r8F66CxKpnbwMEhPS4pwC36V0Kgg",
+  authDomain: "adamas-protocol.firebaseapp.com",
+  databaseURL: "https://adamas-protocol-default-rtdb.firebaseio.com",
+  projectId: "adamas-protocol",
+  storageBucket: "adamas-protocol.firebasestorage.app",
+  messagingSenderId: "207788425238",
+  appId: "1:207788425238:web:025b8544f085dde60af537",
+  measurementId: "G-NVCWQ1XQZS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
