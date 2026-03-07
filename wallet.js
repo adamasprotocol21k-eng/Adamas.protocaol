0aa02ec8e15e89f340aa1dad736e2e7d61b620f5
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-07 16:48:44 +0530

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,1 +1,28 @@
+let provider;
+let signer;
+let userAddress;
 
+document.getElementById("connectWallet").onclick=async()=>{
+
+if(!window.ethereum){
+
+alert("Install MetaMask");
+
+return;
+
+}
+
+await window.ethereum.request({
+method:"eth_requestAccounts"
+});
+
+provider=new ethers.providers.Web3Provider(window.ethereum);
+
+signer=provider.getSigner();
+
+userAddress=await signer.getAddress();
+
+document.getElementById("connectWallet").innerText=
+userAddress.slice(0,6)+"...";
+
+}
