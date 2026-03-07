29c6a441d6f6fb57d20865b0c6defffc8a1aa6ec
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-07 16:49:17 +0530

    Update missions.js

--- missions.js
+++ missions.js
@@ -1,1 +1,37 @@
+let balance=0;
 
+function updateBalance(){
+
+document.getElementById("abpBalance").innerText=balance;
+
+}
+
+function dailyFuel(){
+
+balance+=50;
+
+updateBalance();
+
+alert("Daily Fuel claimed!");
+
+}
+
+function joinTelegram(){
+
+window.open("https://t.me/ADSProtocol");
+
+balance+=100;
+
+updateBalance();
+
+}
+
+function followX(){
+
+window.open("https://x.com");
+
+balance+=100;
+
+updateBalance();
+
+}
