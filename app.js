d6eaceabac90fe80eb9cd985c3f4faf271341c23
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-08 13:52:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-// Firebase Config - APNI DETAILS YAHA BHAREIN
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "PASTE_YOUR_API_KEY",
-    databaseURL: "PASTE_YOUR_DB_URL",
-    projectId: "PASTE_YOUR_PROJECT_ID",
-    appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyCJ2i6r8F66CxKpnbwMEhPS4pwC36V0Kgg",
+  authDomain: "adamas-protocol.firebaseapp.com",
+  databaseURL: "https://adamas-protocol-default-rtdb.firebaseio.com",
+  projectId: "adamas-protocol",
+  storageBucket: "adamas-protocol.firebasestorage.app",
+  messagingSenderId: "207788425238",
+  appId: "1:207788425238:web:025b8544f085dde60af537",
+  measurementId: "G-NVCWQ1XQZS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
