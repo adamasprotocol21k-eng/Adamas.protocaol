2e77ea0ed50acb87a7a315850905b09691bb1f81
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-03-08 13:50:07 +0530

    Update teenpatti.js

--- teenpatti.js
+++ teenpatti.js
@@ -1,14 +1,32 @@
-// Firebase Config (Wahi copy karein jo app.js mein hai)
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "PASTE_YOUR_API_KEY",
-    databaseURL: "PASTE_YOUR_DB_URL",
-    projectId: "PASTE_YOUR_PROJECT_ID",
-    appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyCJ2i6r8F66CxKpnbwMEhPS4pwC36V0Kgg",
+  authDomain: "adamas-protocol.firebaseapp.com",
+  databaseURL: "https://adamas-protocol-default-rtdb.firebaseio.com",
+  projectId: "adamas-protocol",
+  storageBucket: "adamas-protocol.firebasestorage.app",
+  messagingSenderId: "207788425238",
+  appId: "1:207788425238:web:025b8544f085dde60af537",
+  measurementId: "G-NVCWQ1XQZS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
-let userWallet = "0x1E12BFE8eB6Ef1334cF8f873F2d9Abad5C56daDB"; // Default
+let userWallet = "0x6DbC17D9950e0b3A7627ec6bFc6b210A998da690"; // Default
 let currentABP = 0;
 
 // Load Balance
