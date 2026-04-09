8090d87640290e5768c43e8ca46b9d4371a98e05
adamasprotocol21k-eng <adamas.protocol21k@gmail.com>
2026-04-09 18:50:31 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// Adamas Protocol - Configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "your-project.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project.appspot.com",
+    messagingSenderId: "your-id",
+    appId: "your-app-id"
+};
+
+// Isse export karenge taaki baki files use kar sakein
+export default firebaseConfig;
+
